59ca76ed224f59ee5a970e3ab0c86e3af4e6e618
Vinícius Pires Lopes <52144908+ViniciusPiresLopes@users.noreply.github.com>
2020-06-25 21:05:50 -0300

    Update example.py

--- example.py
+++ example.py
@@ -23,7 +23,7 @@ surface = pygame.Surface((10, 10))
 surface.fill((255, 150, 0))
 
 area = Area((WIDTH // 2, HEIGHT // 2), (25, 25))  # Spawn area of the particles
-particle_system = ParticleSystem(area, FPS, 200, 1, [0, 0.5], [5, -12.5], 1)
+particle_system = ParticleSystem(area, FPS, 200, 1, [0, 0.5], [0, -10], 1)
 particle_system.start_particles_with_surface(surface)  # Starting it with a surface | We can use "start_particles(img_path)" for loading images instead
 
 # Main loop
@@ -41,7 +41,7 @@ while run:
     mouse_pos = pygame.mouse.get_pos()
     particle_system.set_pos([*mouse_pos])  # Changes the position of the spawn of the particles (area)
     
-    particle_system.reverse_all()  # For spread particles in the original direction and the opposite of the original direction | seems like mirroring
+    # particle_system.reverse_all()  # For spread particles in the original direction and the opposite of the original direction | seems like mirroring
     
     particle_system.update()  # Update all the physics
     particle_system.draw(win)
